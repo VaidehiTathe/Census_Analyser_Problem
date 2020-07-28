2d3e998aebf20abc73374d3706f8d4683105aee9
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-29 02:28:04 +0530

    [Vaidehi]REFACTOR:testing for incorrect file name exception

--- CensusAnalyser/CSVAnalyserTest/CSVAnalyserUnitTest.cs
+++ CensusAnalyser/CSVAnalyserTest/CSVAnalyserUnitTest.cs
@@ -4,14 +4,14 @@ using CensusAnalyser;
 namespace CSVAnalyserTest
 {
     public class Tests
-    { 
+    {
         [Test]
         public void GivenCSVFile_WhenNumberOfRecordsMatches_ShoulReturnTrue()
         {
             string filePath = @"E:\CodinClub\Assignments_Fellowship\Census_Analyser_Problem\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.csv";
             int CSVRecords = CSVStateCensusAnalyser.GetRecords(filePath);
             int records = StateCensusAnalyser.GetStateCensusRecords(filePath);
-            Assert.AreEqual(CSVRecords,records);
+            Assert.AreEqual(CSVRecords, records);
         }
 
         [Test]
@@ -24,12 +24,28 @@ namespace CSVAnalyserTest
                 int records = CensusAnalyser.StateCensusAnalyser.GetStateCensusRecords(filePath);
                 Assert.AreEqual(CSVRecords, records);
             }
-            catch(StateCensusAnalyserException e)
+            catch (StateCensusAnalyserException e)
             {
                 Assert.AreEqual(StateCensusAnalyserException.ExceptionType.INVALID_PATH, e.Type);
                 //Assert.AreEqual(e.Message,"Entered wrong path");
             }
         }
+        [Test]
+        public void GivenCSVFileName_WhenIncorrectTypeOfFile_ShouldThrowCustomException()
+        {
+            try
+            {
+                string filePath = @"E:\CodinClub\Census_Analyser_Problem\CensusAnalyser\CensusAnalyser\IndiaStateCensusData.txt";
+                int CSVRecords = CSVStateCensusAnalyser.GetRecords(filePath);
+                int records = StateCensusAnalyser.GetStateCensusRecords(filePath);
+                Assert.AreEqual(CSVRecords, records);
+            }
+            catch (StateCensusAnalyserException e)
+            {
 
+                Assert.AreEqual("Name of the file is incorrect", e.Message);
+            }
+
+        }
     }
 }
