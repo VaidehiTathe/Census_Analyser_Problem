670e2ead6b5ebc21d5b03c63398956e94b6a320d
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-29 02:12:29 +0530

    [Vaidehi]REFACTOR:handling exception of file name

--- CensusAnalyser/CensusAnalyser/CSVStateCensusAnalyser.cs
+++ CensusAnalyser/CensusAnalyser/CSVStateCensusAnalyser.cs
@@ -24,6 +24,11 @@ namespace CensusAnalyser
             {
                 throw new StateCensusAnalyserException("Give correct path");
             }
+
+            catch (FileNotFoundException)
+            {
+                throw new StateCensusAnalyserException("Incorrect file name");
+            }
         }
     }
 }
